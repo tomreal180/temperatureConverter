010c10199b7e7912ef676c482cc624183d471009
Minh Duc Ha <duc.ha@alumni.fh-aachen.de>
2025-03-11 15:47:51 +0100

    Update: Add base code and caculation

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,5 +1,56 @@
 <template>
-    <p>This is Converter</p>
+    <form 
+    @submit.prevent="handleSubmit"
+    class="flex flex-col"
+    >
+        <label for="value">Degrees</label>
+        <input name="value" type="number" v-model="value">
+        <label for="types">Type</label>
+        <select name="types" v-model="type">
+            <option value="f">Fahrenheit</option>
+            <option value="k">Kelvin</option>
+        </select>
+        <input type="submit" value="Convert">
+    </form>
+    <label for="result">Result</label>
+    <p name="result">{{ result }}</p>
+
 </template>
-<script></script>
+<script>
+export default{
+    data() {
+        return {
+            type:'f',
+            value: 0,
+            result: '',
+        };
+    },
+    methods: {
+        handleSubmit() {
+            if(this.type === 'f'){
+                this.result = this.convertFtoC().toFixed(2)+' °C'
+            } else if (this.type === 'k'){
+                this.result = this.convertKtoC().toFixed(2)+' °C'
+            }
+        },
+        convertFtoC() {
+            return (this.value-32)*5/9;
+        },
+        convertKtoC(){
+            return this.value-273.15;
+        }
+    },
+    watch: {
+        value(val){
+            if(this.type === 'k'){
+                if (val<0){
+                    this.value = 0
+                }else if (val>10e32){
+                    this.value = 10e32
+                }
+            }
+        }
+    }
+};
+</script>
 <style></style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import Converter from './components/Converter.vue'
 import App from './App.vue'
@@ -5,3 +6,5 @@ import App from './App.vue'
 const app= createApp(App)
 app.component('converter', Converter)
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
